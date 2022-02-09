750239299e2e38fe7fe6cc18f18306e7429a5fdc
Fisayo Oluwayemi (Dev Fishi) <fisayo@wellahealth.com>
2022-02-09 12:02:12 +0100

    added Farm Model class

---
+++ Models/Farm.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace AgricFameAPICosmosDB.Models
+{
+    public class Farm
+    {
+
+        //Azure Cosmos uses JSON to store Data
+        [JsonProperty(PropertyName = "id")]
+        public int Id { get; set; }
+
+        [JsonProperty(PropertyName = "cropName")]
+        public string CropName { get; set; }
+
+        [JsonProperty(PropertyName = "cropDescription")]
+        public string CropDescription { get; set; }
+    }
+}
